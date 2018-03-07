a7afda2493725e5206e43ae982d51ceda725a231
Elvis Shi <shonetesmith@gmail.com>
2018-03-07 16:06:20 +0900

    Create blend-s.rb

---
+++ blend-s.rb
@@ -0,0 +1,13 @@
+ class BlendS < Formula
+  desc "Blend S Meme (\"S\" stands for??)"
+  homepage ""
+  url "https://github.com/el-hoshino/blend-s/archive/version/1.0.0.tar.gz"
+  sha256 "1f6272f7f264db7c39b5d294299bf107a1c793fe25a05c8fe24719c51a237257"
+   def install
+    system "xcodebuild -project blend-s/blend-s.xcodeproj"
+    bin.install "blend-s/build/Release/blend-s"
+  end
+   test do
+    system "false"
+  end
+end
